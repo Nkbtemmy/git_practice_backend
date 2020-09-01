10ce134d63ba2c927c1f1c855157d9e613cc801d
unknown <emmanuelnkubito2@gmail.com>
2020-09-01 21:23:37 +0200

    updating time format

--- models/blog.model.js
+++ models/blog.model.js
@@ -4,7 +4,7 @@ const blogSchema = mongoose.Schema({
     author: String,
     content: String,
     comments: [{ body: String, date: Date }],
-    date: { type: Date, default: new Date().toLocaleString() },
+    date: { type: Date, default: Date.now },
     hidden: Boolean
 }, {
     timestamps: true
